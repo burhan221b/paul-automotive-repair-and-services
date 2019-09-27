8a99cb6abc0560cee7e307f8098f88a86c035382
Burhan <burhan221b@gmail.com>
2019-09-27 02:51:54 -0700

    Added inital css and html for Nav component. The responsive design and clicking the nav button works. Need to add transition animation to make things smoother.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,16 @@ import "../scss/Nav.scss";
 const Nav = () => {
     return (
         <header>
-
+            <div className="nav">
+                <label htmlFor="toggle">&#9776;</label>
+                <input type="checkbox" id="toggle" />
+                <div className="menu">
+                    <NavLink to="/" exact >Home</NavLink>
+                    <NavLink to="/services" exact>Services</NavLink>
+                    <NavLink to="/about" exact>About</NavLink>
+                    <NavLink to="/contact" exact>Contact</NavLink>
+                </div>
+            </div>
         </header >
     )
 }
