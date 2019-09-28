f1975eb80364ac8b510f0c816ba0c8ef9a01efa7
Burhan <burhan221b@gmail.com>
2019-09-28 01:39:29 -0700

    Added some changes in components. I was able to add logo and responsive designs. Works well with mobile devices as well. ONLY issue at the moment is menu doesn't close when link is cliked. May have to use Javascript later. I think i'm dome with navagation, Maybe clean up css code and then move on to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../scss/Home.scss";
 
 export default function Home() {
     return (
         <div>
-            <h1>hello</h1>
+            <h1><a href="#">hello</a></h1>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,14 @@ const Nav = () => {
     return (
         <header>
             <div className="nav">
+                {/* For Logo and Animation */}
+                <div id='logo'>
+                    <NavLink className="" to="/" exact>
+                        <img className='animated fadeInDown' src="./Images/logo/logo.png" alt="Paul Nguyen Automotive Repair Service" />
+                    </NavLink>
+                    <h2 className='title'>Complete Automotive Repair<br /><span className="red">(408)245-2122</span></h2>
+                </div>
+                {/* All for Navigation links and Toggle */}
                 <label htmlFor="toggle">&#9776;</label>
                 <input type="checkbox" id="toggle" />
                 <div className="menu">
