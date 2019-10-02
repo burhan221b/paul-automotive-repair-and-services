148de7d0e5fd9d6cd4c5ae656232bd7b77e70d2b
Burhan <burhan221b@gmail.com>
2019-10-01 18:31:26 -0700

    Set up home page html and some scss. Note: might be changing the set up as i go on. Including some animations if it fits well.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,18 @@ import "../scss/Home.scss";
 
 export default function Home() {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="main-grid">
+            {/* Hero-image */}
+            <section className="grid-1 hero-image"></section>
+            {/* Services */}
+            <section className="grid-2"></section>
+            {/* Location & About */}
+            <section className="grid-3"></section>
+            {/* Footer & Footer-image */}
+            <footer className="grid-4 main-footer">
+                <section className="footer-1 footer-image"></section>
+                <section className="footer-2"></section>
+            </footer>
         </div>
     )
 }
