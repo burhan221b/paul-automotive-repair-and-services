801c401eb7e7eef14f1b45809b09c74d7d26b49b
Burhan <burhan221b@gmail.com>
2019-10-23 23:48:38 -0700

    Changed a lot of things for navbar. So i removed the logo, included a texted based logo with an icon of wrench for the logo. Hopefully also good for SEO. I changed the css to include flexbox. So some of the floats were removed infavor for flexbox. Testing the UI, it seemed the layout wasn't broken and everything worked well. Probably will merge this to master branch now.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { MdPhone, MdEmail } from 'react-icons/md';
+import { FaWrench } from 'react-icons/fa';
+
+
 
 
 // Stylesheet
@@ -20,22 +23,24 @@ class Nav extends React.Component {
             <header>
                 <div className="info-banner">
                     <ul>
-                        <li className="phone"><a href="tel:4082452122"><MdPhone /> <span className="red">(408)245-2122</span></a></li>
-                        <li className="email"><a href="mailto:nguyen.paul1965@gmail.com" target="_top"><MdEmail /> <span className="red">nguyen.paul1965@gmail.com</span></a></li>
                         <li>
-                            <ul>
+                            {/* <ul>
                                 <li>ASE</li>
                                 <li>Star Smog</li>
-                            </ul>
+                            </ul> */}
                         </li>
+                        <li className="phone"><a href="tel:4082452122"><MdPhone /> <span className="red">(408)245-2122</span></a></li>
+                        <li className="email"><a href="mailto:nguyen.paul1965@gmail.com" target="_top"><MdEmail /> <span className="red">nguyen.paul1965@gmail.com</span></a></li>
+
                     </ul>
                 </div>
                 <div className="nav">
                     {/* For Logo and Animation */}
                     <div id='logo'>
                         <NavLink className="" to="/" exact>
-                            {/* <img className='animated fadeInDown' src="./Images/logo/logo.png" alt="Paul Nguyen Automotive Repair Service" /> */}
-                            <h1>Paul's Automotive Repair and Services</h1>
+                            {/* <img className='animated fadeInDown' src="./Images/logo/logo5.png" alt="Paul Nguyen Automotive Repair Service" /> */}
+                            <h1><span className="blue">Paul<FaWrench className="wrench" /> s</span></h1>
+                            <h2>Automotive Repair and Services</h2>
                         </NavLink>
                         {/* <h2 className='title'>Complete Automotive Repair<br /><a href="tel:4082452122"><span className="red">(408)245-2122</span></a></h2> */}
                     </div>
