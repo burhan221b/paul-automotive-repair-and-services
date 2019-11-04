736f3fea2e2cbdf18a5d455ac8beb8571ff30d84
Burhan <burhan221b@gmail.com>
2019-11-03 18:04:23 -0800

    adding google console

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Home from './components/Home';
 import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
+import VerifyGoogle from './components/VerifyGoogle';
 
 function initializeAnalytics() {
   ReactGA.initialize('UA-150659608-1');
@@ -25,15 +26,16 @@ const App = () => {
     <Router>
       <div className="App">
         <Nav />
-        {/* <Switch> */}
-        <Route path="/" exact component={Home} />
-        {/* <Route path="/services/:id" exact component={Home} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/google8f61f935e8518ee6.html" exact component={VerifyGoogle} />
+          {/* <Route path="/services/:id" exact component={Home} />
           <Route path="/about/:id" exact component={Home} />
           <Route path="/contact/:id" exact component={Home} /> */}
-        {/* <Route path="/services/:id" exact component={Services} />
+          {/* <Route path="/services/:id" exact component={Services} />
           <Route path="/about/:id" exact component={About} />
           <Route path="/contact/:id" exact component={Contact} /> */}
-        {/* </Switch> */}
+        </Switch>
       </div>
 
     </Router>
---
+++ src/components/VerifyGoogle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VerifyGoogle() {
+    return (
+        <>
+            google-site-verification: google8f61f935e8518ee6.html
+        </>
+    )
+}
